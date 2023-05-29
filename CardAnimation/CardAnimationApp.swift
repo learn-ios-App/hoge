e591288434c6992bc9af1e60d6c98610bac8ccf2
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-29 22:38:36 +0900

    Initial Commit

---
+++ CardAnimation/CardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAnimationApp.swift
+//  CardAnimation
+//
+//  Created by 渡邊魁優 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct CardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
